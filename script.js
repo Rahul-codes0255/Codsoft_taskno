05fe65fc34d7cf4924e79a43d454bdd9e25b3958
Rahul-codes0255 <rahulneelam814@gmail.com>
2025-08-26 21:28:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("resumeDownload").addEventListener("click", function (e) {
-  e.preventDefault();
-  // Replace with actual resume file path
-  const resumeUrl = "resume.pdf"; 
-  window.open(resumeUrl, "_blank");
-});
